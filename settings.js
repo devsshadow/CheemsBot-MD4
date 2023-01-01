b550278c14662a15d46a4432e83931eff3f56b9c
devsshadow <98581562+devsshadow@users.noreply.github.com>
2023-01-01 11:31:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['94764379595'] //ur owner number
+global.ownername = "Devs Shadow⚡" //ur owner name
+global.ytname = "YT: Shadow del Zap" //ur yt chanel name
+global.socialm = "Instagram: https://instagram.com/devs_shadow" //ur github or insta name
+global.location = "Sri Lanka" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.owner = ['94764379595']
+global.ownertag = '94764379595' //ur tag number
+global.botname = 'Shadow Bot' //ur bot name
+global.linkz = "https://youtube.com/@ShadowdelZap" //your theme url which will be displayed on whatsapp
+global.websitex = "https://instagram.com/devs_shadow" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.reactmoji = "⚡" //ur menu react emoji
+global.themeemoji = "✨" //ur theme emoji
+global.packname = "Shadow Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "Devs Shadow⚡" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
